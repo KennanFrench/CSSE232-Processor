bff64db19611c40f9c85ecc24d0bf8e003acae62
peterseo <peterseo@80a3961c-da37-4da7-b2c9-e1b3553d226c>
2015-10-19 13:22:31 +0000

    4-bit, 8-bit, 12-bit sign extensions and an 8-bit to 16-bit load upper logic. Also has a 16-bit buffer.

---
+++ XilinxProject/SignExtensions/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "/home/ethan/Downloads/SignExtensions/test_signext_test_signext_sch_tb_isim_beh.exe" -prj "/home/ethan/Downloads/SignExtensions/test_signext_test_signext_sch_tb_beh.prj" "work.test_signext_test_signext_sch_tb" "work.glbl" 
